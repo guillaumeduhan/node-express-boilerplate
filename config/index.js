2b4b5e150a388b629f89de3109fcdee5d726e5c2
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-18 21:41:31 +0100

    index

--- config/index.js
+++ config/index.js
@@ -5,8 +5,4 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   databaseURL: process.env.DATABASE_URI,
-  mailchimp: {
-    apiKey: process.env.MAILCHIMP_API_KEY,
-    sender: process.env.MAILCHIMP_SENDER,
-  }
 }
