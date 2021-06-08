b5e7135bc6847751e664fe0181c4b489459cdd82
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2021-06-08 23:52:23 +0100

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
+const Hubspot = require('hubspot')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('403 Unauthorized.')
+})
+
+app.get('/createOrUpdate', function (req, res) {
+  const hubspot = new Hubspot({
+    apiKey: 'e68542ee-c8b4-4dd9-8825-b979777ff6d1',
+    checkLimit: false
+  })
+  hubspot.contacts.createOrUpdate('gduhan@test.com', {
+    "properties": [
+      { "property": "firstname", "value": 'hello' }
+    ]
+  })
+    .then((data) => {
+      res.send(data)
+    })
+    .catch((err) => {
+      res.send(err)
+    })
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Gilbert Hubspot app running on 3000!')
 })
